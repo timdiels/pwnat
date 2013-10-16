041eb2fecbbe73d6ab0c409de8a9142466ba1da0
timdiels <timdiels.m@gmail.com>
2013-10-16 20:47:25 +0200

    Implemented argument parsing

--- src/pwnat/common.h
+++ src/pwnat/common.h
@@ -19,22 +19,6 @@
 
 #pragma once
 
-#include <limits>
-
-#define NO_DEBUG     0
-#define DEBUG_LEVEL1 1
-#define DEBUG_LEVEL2 2
-#define DEBUG_LEVEL3 3
-
-// TODO rm
-extern int opt_debug;
-
-#ifdef WIN32
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-#endif
-
 /* cl.exe has a different 'inline' keyword for some dumb reason */
 #ifdef WIN32
 #define _inline_ __inline
@@ -42,24 +26,3 @@ typedef unsigned int uint32_t;
 #define _inline_ inline
 #endif
 
-// TODO rm
-#define PERROR_GOTO(cond,err,label){        \
-        if(cond)                            \
-        {                                   \
-                perror(err) ;               \
-            goto label;                     \
-        }}
-
-            //if(debug_level >= DEBUG_LEVEL1)
-
-#define ERROR_GOTO(cond,str,label){                  \
-        if(cond)                                     \
-        {                                            \
-            if(debug_level >= DEBUG_LEVEL2)          \
-                fprintf(stderr, "Error: %s\n", str); \
-            goto label;                              \
-        }}
-
-// TODO rm
-#define MAX(a,b) ((a) > (b) ? (a) : (b))
-#define MIN(a,b) ((a) < (b) ? (a) : (b))
--- src/pwnat/pwnat.cpp
+++ src/pwnat/pwnat.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009, 2010 by Daniel Meekins, Samy Kamkar
+ * Copyright (C) 2009, 2010, 2013 by Daniel Meekins, Samy Kamkar, Tim Diels
  *
  * This file is part of pwnat.
  *
@@ -17,103 +17,147 @@
  * along with pwnat.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*
- * This project is based off of udptunnel (most files originate from that project)
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-
-#ifndef WIN32
-#include <unistd.h>
-#else
-#include "xgetopt.h"
-#endif
-
 #include "common.h"
 
-int opt_debug = 0;
+#include <iostream>
+#include <string>
+#include <boost/program_options.hpp>
+#include <pwnat/accumulator.hpp>
 
-int debug_level = 1; //NO_DEBUG;
-//int ipver = SOCK_IPV4;
+#include "client/TCPServer.h"
+#include "server/ProxyServer.h"
 
-int udpclient(int argc, char *argv[]);
-int udpserver(int argc, char *argv[]);
-void usage(char *progname);
+using namespace std;
+namespace po = boost::program_options;
 
-int main(int argc, char *argv[])
-{
-    int ret;
-    int isserv = 0;
+struct Args {
+    bool is_server;
+    bool is_ipv6; // if not, it's ipv4
+    int verbosity;
+    std::string bind_address; // TODO This has to do with binding of serving sockets. Defaults to 0.0.0.0 or ::, is an ip not a dns string
+    u_int16_t proxy_port;
 
-#ifdef WIN32    
-    WSADATA wsa_data;
-    ret = WSAStartup(MAKEWORD(2,0), &wsa_data);
-    ERROR_GOTO(ret != 0, "WSAStartup() failed", error);
-#endif
-    
-    while((ret = getopt(argc, argv, "hscv6")) != EOF)
-    {
-        switch(ret)
-        {
-            case '6':
-                //ipver = SOCK_IPV6;
-                break;
-                
-            case 's':
-                isserv = 1;
-                break;
-                
-            case 'c':
-                isserv = 0;
-                break;
+    // client only args
+    u_int16_t local_port;
+    std::string proxy_host; // TODO can be a dns string, resolve locally
+    std::string remote_host; // TODO can be a dns string, resolve remotely
+    u_int16_t remote_port;
+};
 
-            case 'v':
-                if(debug_level < 3)
-                    debug_level++;
-                break;
-                
-            case 'h':
-                /* fall through */
-            default:
-                goto error;
+void parse_args(int argc, char *argv[], Args& args);
+
+int main(int argc, char *argv[]) {
+    Args args;
+
+    try {
+        parse_args(argc, argv, args);
+    }
+    catch (const exception& e) {
+        cerr << "Parse error: " << e.what() << endl;
+        return 1;
+    }
+
+    try {
+        if(!args.is_server) {
+            // client
+            TCPServer client;
+            client.run();
+        }
+        else {
+            // server
+            ProxyServer server;
+            server.run();
+        }
+
+    }
+    catch (const exception& e) {
+        cerr << e.what() << endl;
+        abort();
+    }
+
+    cout << "Main thread exiting" << endl;
+    return 0;
+}
+
+void print_usage(po::options_description& options_spec) {
+    cout << "pwnat client-mode listens for TCP traffic client-side and forwards it to a pwnat proxy server." << endl
+         << endl
+         << "pwnat server-mode forwards data of connected proxy clients to the remote host specified by the client. pwnat server forwards replies to appropriate client. Multiple proxy and TCP clients are supported. Proxy client and server may both be behind a NAT." << endl
+         << endl
+         << "Synopsis:" << endl
+         << "  pwnat -c <options> <local port> <proxy host> <remote host> <remote port>" << endl
+         << "  pwnat -s <options>" << endl
+         << endl
+         << options_spec << endl;
+}
+
+void parse_args(int argc, char *argv[], Args& args) {
+    po::options_description options_spec("General Options");
+    options_spec.add_options()
+        ("help,h", po::bool_switch(), "show help and exit")
+        ("server,s", po::bool_switch(&args.is_server), "server mode")
+        ("client,c", po::bool_switch(), "client mode")
+        ("ipv6,6", po::bool_switch(&args.is_ipv6), "use IPv6")
+        ("verbose,v", accumulator<int>(&args.verbosity)->implicit_value(1), "increase verbosity")
+        ("bindaddress,b", po::value<string>(&args.bind_address)->default_value(""), "local IP to bind to")
+        ("proxyport,p", po::value<u_int16_t>(&args.proxy_port)->default_value(2222), "proxy server port") // TODO is default noted in usage output?
+    ;
+
+    po::options_description client_specific_options("Client Options");
+    client_specific_options.add_options()
+        ("localport", po::value<u_int16_t>(&args.local_port), "local TCP port to listen on")
+        ("proxyhost", po::value<string>(&args.proxy_host), "proxy host dns/ip")
+        ("remotehost", po::value<string>(&args.remote_host), "remote server dns/ip, resolved on proxy server.")
+        ("remoteport", po::value<u_int16_t>(&args.remote_port), "remote port")
+    ;
+
+    po::positional_options_description positional_options; // maps positional options to regular options
+    positional_options
+        .add("localport", 1)
+        .add("proxyhost", 1)
+        .add("remotehost", 1)
+        .add("remoteport", 1)
+    ;
+
+    options_spec.add(client_specific_options);
+
+    po::variables_map vars;
+    po::store(po::command_line_parser(argc, argv).options(options_spec).positional(positional_options).run(), vars);
+    po::notify(vars);
+
+    if (vars["help"].as<bool>()) {
+        print_usage(options_spec);
+        exit(1);
+    }
+
+    if (!(vars["server"].as<bool>() xor vars["client"].as<bool>())) {
+        throw runtime_error("Need to specify one of --server and --client");
+    }
+
+    // client only args
+    if (!args.is_server) {
+        if (!vars.count("localport")) {
+            throw runtime_error("Need to specify --localport");
+        }
+        if (!vars.count("proxyhost")) {
+            throw runtime_error("Need to specify --proxyhost");
+        }
+        if (!vars.count("remotehost")) {
+            throw runtime_error("Need to specify --remotehost");
+        }
+        if (!vars.count("remoteport")) {
+            throw runtime_error("Need to specify --remoteport");
         }
     }
 
-    ret = 0;
-    
-    if(isserv)
-    {
-        if(argc - optind < 0)
-            goto error;
-        ret = udpserver(argc - optind, argv + optind);
-    }
-    else
-    {
-        if(argc - optind != 5 && argc - optind != 6 && argc - optind != 4)
-            goto error;
-        ret = udpclient(argc - optind, argv + optind);
-    }
-
-#ifdef WIN32
-    WSACleanup();
-#endif
-    
-    return ret;
-    
-  error:
-    usage(argv[0]);
-    exit(1);
+    cout<< "serv" << args.is_server << endl 
+        << "ipv6" << args.is_ipv6 << endl 
+        << "v" << args.verbosity << endl 
+        << "bind" << args.bind_address << endl 
+        << "pport" << args.proxy_port << endl
+        << "lport" << args.local_port << endl
+        << "phost" << args.proxy_host << endl
+        << "rhost" << args.remote_host << endl
+        << "rport" << args.remote_port << endl;
 }
 
-void usage(char *progname)
-{
-    printf("usage: %s <-s | -c> <args>\n", progname);
-    printf("  -c    client mode (default)\n"
-           "        <args>: [local ip] <local port> <proxy host> [proxy port (def:2222)] <remote host> <remote port>\n"
-           "  -s    server mode\n"
-           "        <args>: [local ip] [proxy port (def:2222)] [[allowed host]:[allowed port] ...]\n"
-           "  -6    use IPv6\n"
-           "  -v    show debug output (up to 2)\n"
-           "  -h    show this help and exit\n");
-}
